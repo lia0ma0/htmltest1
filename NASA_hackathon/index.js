f1e78752e1753537f4be905523f07e2bcb727f2c
lia0ma0 <114229245+lia0ma0@users.noreply.github.com>
2022-10-10 14:44:48 +0800

    Add files via upload

---
+++ NASA_hackathon/index.js
@@ -0,0 +1,220 @@
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btn').onclick = function ()
+                {
+                    copyText(document.querySelector('#text'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btni').onclick = function ()
+                {
+                    copyText(document.querySelector('#texti'));
+                }
+                
+
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btntwo').onclick = function ()
+                {
+                    copyText(document.querySelector('#texttwo'));
+                }
+    
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnthree').onclick = function ()
+                {
+                    copyText(document.querySelector('#textthree'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnfour').onclick = function ()
+                {
+                    copyText(document.querySelector('#textfour'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnfive').onclick = function ()
+                {
+                    copyText(document.querySelector('#textfive'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnsix').onclick = function ()
+                {
+                    copyText(document.querySelector('#textsix'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnseven').onclick = function ()
+                {
+                    copyText(document.querySelector('#textseven'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btneight').onclick = function ()
+                {
+                    copyText(document.querySelector('#texteight'));
+                }
+                
+function copyText(htmlElement)
+                {
+                    if (!htmlElement) {
+                        return;
+                    }
+                    
+                    let elementText = htmlElement.innerText;
+                    
+                    let inputElement = document.createElement('input');
+                    inputElement.setAttribute('value', elementText);
+                    document.body.appendChild(inputElement);
+                    inputElement.select();
+                    document.execCommand('copy');
+                    inputElement.parentNode.removeChild(inputElement);
+                    
+                }
+                
+                document.querySelector('#copy-text-btnnine').onclick = function ()
+                {
+                    copyText(document.querySelector('#textnine'));
+                }
